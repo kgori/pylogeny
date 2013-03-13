960c5bc0b35c50a336120c27e0b29b6ac45b7118
kgori <kgori@ebi.ac.uk>
2013-03-13 15:22:22 +0000

    rangecheck

--- errors.py
+++ errors.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python
 
-
 import os
 import sys
 __all__ = [
     'FileError',
     'DirectoryError',
     'OptionError',
-    'filecheck_and_raise',
-    'filecheck_and_quit',
-    'directorycheck_and_make',
-    'directorycheck_and_raise',
-    'directorycheck_and_quit',
-    'optioncheck_and_raise',
+    'RangeError',
+    'filecheck',
+    'filequit',
+    'directorymake',
+    'directorycheck',
+    'directoryquit',
+    'optioncheck',
+    'rangecheck',
     ]
+
+# Constants
+
+POSINF = float('inf')
+NEGINF = -POSINF
 
 
 class FileError(Exception):
 
-    """
-    Reporting when file errors occur
-    """
+    """ Reporting when file errors occur """
 
     def __init__(self, filename=''):
-        """
-        Store the file that gave the exception
-        """
+        """ Store the file that gave the exception """
 
         self.value = filename
 
@@ -35,49 +37,68 @@ class FileError(Exception):
 
 class DirectoryError(FileError):
 
-    """
-    Reporting when directory errors occur
-    """
+    """ Reporting when directory errors occur """
 
     def __str__(self):
         line1 = 'Error opening directory \'{0}\''.format(self.value)
         line2 = 'Directory doesn\'t exist'
         return '\n'.join((line1, line2))
 
+
 class OptionError(Exception):
 
-    """
-    Reports on disallowed options passed to functions
-    """
+    """ Reports on disallowed options passed to functions """
 
     def __init__(self, option, choices):
         self.value = option
         self.choices = choices
 
     def __str__(self):
-        return '\'{0}\' is not a valid option. Valid options are {1}'.format(self.value, self.choices)
+        return '\'{0}\' is not a valid option. Valid options are {1}'.format(self.value,
+                self.choices)
 
-def filecheck_and_raise(filename):
+
+class RangeError(Exception):
+
+    """ Raise exception when number is outside a valid range """
+
+    def __init__(
+        self,
+        n,
+        lower,
+        upper,
+        ):
+        self.value = n
+        self.lower = lower
+        self.upper = upper
+
+    def __str__(self):
+
+        return '\'{0}\' is outside the valid range {1} - {2}'.format(self.value,
+                self.lower, self.upper)
+
+
+def filecheck(filename):
     if not os.path.isfile(filename):
         raise FileError(filename)
     return filename
 
 
-def filecheck_and_quit(filename):
+def filequit(filename):
     try:
-        filecheck_and_raise(filename)
+        filecheck(filename)
     except FileError, e:
         print e
         sys.exit()
 
 
-def directorycheck_and_raise(directory):
+def directorycheck(directory):
     if not os.path.isdir(directory):
         raise DirectoryError(directory)
     return directory
 
 
-def directorycheck_and_quit(directory):
+def directoryquit(directory):
     try:
         if not os.path.isdir(directory):
             raise DirectoryError(directory)
@@ -86,18 +107,24 @@ def directorycheck_and_quit(directory):
         sys.exit()
 
 
-def directorycheck_and_make(directory, verbose=True):
+def directorymake(directory, verbose=True):
     try:
-        directorycheck_and_raise(directory)
+        directorycheck(directory)
     except DirectoryError, e:
         if verbose:
             print e
             print 'Creating \'{0}\''.format(directory)
         os.makedirs(directory)
-    return directorycheck_and_raise(directory)
+    return directorycheck(directory)
 
 
-def optioncheck_and_raise(option, choices):
+def optioncheck(option, choices):
     if option not in choices:
         raise OptionError(option, choices)
     return option
+
+
+def rangecheck(n, lower, upper):
+    if n < lower or n > upper:
+        raise RangeError(n, lower, upper)
+    return n
