6eb5152e3f6df53121d2f7c50f73ea6845d72df7
Kevin Gori <kgori@ebi.ac.uk>
2013-06-11 23:25:28 +0100

    Added lognormal parameter conversion function to utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from numpy import log, sqrt
 
 def flatten_list(list_of_lists):
     """ This is faster than the one-liner version:-
@@ -11,3 +12,22 @@ def flatten_list(list_of_lists):
     for sublist in list_of_lists:
         x(sublist)
     return flat_list
+
+def lognormal_parameters(mean, stdev):
+    """ 
+    Given the mean and standard deviation wanted from a lognormal distribution,
+    returns the mean (mu) and standard deviation (sigma) of the underlying
+    normal distribution.
+    This is useful for generating lognormal samples -
+        params = lognormal_parameters(m, s)
+        sample = numpy.random.lognormal(*params, size=N)
+        [sample.mean()=m; sample.std()=s]
+
+    Alternatively, sample = [random.lognormvariate(*params) for _ in range(N)]
+    """
+    mean=float(mean)
+    stdev=float(stdev)
+    variance = stdev**2
+    sigma_sq = log( 1 + (variance/mean**2) )
+    mu = log(mean) - sigma_sq/2
+    return mu, sqrt(sigma_sq)
